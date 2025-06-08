76a6f96dacad51215ee42ed4448fb78b62f40934
unknown <martdev1963@gmail.com>
2025-06-09 00:07:00 +0100

    add functionality to weather app, fetching user lat/lon works but ...get weather functionality still glitchy

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 Flask Weather App that gets waether by city using the OpenWeather API.
 
+ver: 1.02.01 -
+includes enhanced css styling and functionality that supports user location specific weather forcast data...
+Has a clean, modern look
+Lets users type a city (handled by original function route)
+or click "Use My Location" to fetch local weather (handled by new function route)
+
 $ flask --version
 Python 3.12.0
 Flask 3.0.0
 Werkzeug 3.0.1
 
-https://openweathermap.org/city/2643743
+API Endpoint:
+https://openweathermap.org/city/
--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template, request
 import requests
 
+# instantiate a Flask object by calling the Flask constructor...
 app = Flask(__name__)
 
-API_KEY = "ec6f7054139c2b23cc945f009ffb381a"
+# should hide the api key inside an environment variable to avoid 'Slacker' exploits...
+API_KEY = "80189448ac9b9dfdace19c6e4e55b826"
+
+"""
+--------------------------------------------------------------------------------------------
+                                    **FUNCTION ROUTES**
+
+index() - sends request to API
+weather_by_location() - sends request to API with user coords data
+to get user's local weather forcast...
+
+--------------------------------------------------------------------------------------------
+"""
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -19,7 +32,7 @@ def index():
             temp_c = data['main']['temp'] # celsius is the default...
             temp_f = round((temp_c * 9/5) + 32, 2) # convert to farenheit...
             
-            # weather object...
+            # weather dict...(like a js object)
             weather = {
                 'city': city,
                 #'temperature': data['main']['temp'], <------ celsius is the default...
@@ -34,6 +47,55 @@ def index():
     return render_template('index.html', weather=weather)        
 
 
+# ----Code Refactor START----
+# This function route is a new and improved index() function....its pulling the same index.html template file below...
+
+@app.route('/weather')
+def weather_by_location():
+    lat = request .args.get('lat')
+    lon = request .args.get('lon')
+    print("→ Got lat/lon from request:", lat, lon)
+    weather = {} # weather dict...
+    
+    # exploiting and/or using the other parameters the API offers; -----> lat={lat}&lon={lon}&appid={API_KEY}&units=metric...
+    if lat and lon:
+        url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json() # convert to json object and parse the data below...
+            print("→ Weather API response:", data)
+            if 'main' in data and 'weather' in data:
+                temp_c = data['main']['temp']
+                temp_f = round((temp_c * 9/5) + 32, 2) #  convert to farenheit and round off to 2 decimal places...
+
+                # fill the weather dict declared above with key : value pairs...
+                weather = {
+                    'city' : data['name'],
+                    'temp_c' : round(temp_c, 2),  
+                    'temp_f' : temp_f, 
+                    'description' : data['weather'][0]['description'], 
+                    'icon' : data['weather'][0]['icon'] 
+                }
+            else:
+                print("⚠️ Weather API returned unexpected format.")
+                weather = {'error': 'Unable to retrieve weather data.'}
+        else:
+            print("❌ API request failed:", response.status_code)
+            weather = {'error': 'Location not found'}          
+    else:
+            print("⚠️ No lat/lon provided in request.")
+            weather = {'error': 'Missing coordinates'}
+    return render_template('index.html', weather=weather) # including the weather dict, passing it as a template keyword argument...            
+
+
+
+
+
+
+
+# ----Code Refactor END----
+
+# this code auto-restarts flask server after any new edits of code...
 if __name__ == '__main__':
     app.run(debug=True)
 
